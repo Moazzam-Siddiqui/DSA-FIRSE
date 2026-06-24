5684d475a544681c0f512388e761be60d97435ea
Moazzam Ahmad Siddiqui <moazzamdubai12345@gmail.com>
2026-06-24 21:31:13 +0530

    Aajka Dusra Question

---
+++ Striver/CheckSorted.java
@@ -0,0 +1,22 @@
+package Striver;
+
+public class CheckSorted{
+
+    public static  boolean sol(int[] arr){
+        for(int i = 0; i < arr.length;i++ ){
+            for(int j = i +1; j < arr.length;j ++){
+                if(arr[j] < arr[i]){
+                    return false;
+                }
+            }
+        }return true;
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,6,8};
+        int[] arr2 = {9,3,4,7,8};
+
+        System.out.println(sol(arr));
+        System.out.println(sol(arr2));
+    }
+}
